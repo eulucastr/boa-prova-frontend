a8a8a0a1da8fbee85f5c68a3d4a21ec65ba26d4a
Lucas Torres <eu.lucastr@gmail.com>
2026-03-10 22:03:58 -0300

    feat: add home page and google btn

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+export default async function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <button>
+        <a href="http://localhost:3333/oauth2/google">
+            Google Login
+        </a>
+      </button>
+    </div>
+  );
+}
